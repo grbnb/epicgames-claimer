944e96c0363214502b9b32c526d40e1598ce866c
luminoleon <luminoleon@outlook.com>
2021-09-20 09:16:31 +0800

    不用api检查是否登录

--- epicgames_claimer.py
+++ epicgames_claimer.py
@@ -121,7 +121,7 @@ class epicgames_claimer:
             if self.headless:
                 await self._headless_stealth_async()
             self.browser_opened = True
-        await self._refresh_cookies_async()
+        # await self._refresh_cookies_async()
 
     async def _refresh_cookies_async(self) -> None:
         await self._navigate_async("https://www.epicgames.com/store/en-US/")
@@ -289,7 +289,7 @@ class epicgames_claimer:
                 alert_text = await self._get_text_async("#modal-content div[role*=alert]")
                 raise PermissionError("From Epic Games: {}".format(alert_text))
 
-    async def _need_login_async(self, use_api: bool = True) -> bool:
+    async def _need_login_async(self, use_api: bool = False) -> bool:
         if use_api:
             page_content_json = await self._get_json_async("https://www.epicgames.com/account/v2/ajaxCheckLogin")
             return page_content_json["needLogin"]
